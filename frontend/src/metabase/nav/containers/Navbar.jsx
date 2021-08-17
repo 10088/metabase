10088/metabase

commit e49aea2f0c4ead713f08eb6a5b87b6375f1cbfe0

Avoid prop shorthand in the navbar (#17460)

frontend/src/metabase/nav/containers/Navbar.jsx
@@ -216,7 +216,7 @@ export default class Navbar extends Component {
           </Link>
         </Flex>
         <Flex className="flex-full z1" pr={2} align="center">
-          <Box w={1} style={{ maxWidth: 500 }}>
+          <Box width={1} style={{ maxWidth: 512 }}>
             <SearchBar
               location={this.props.location}
               onChangeLocation={this.props.onChangeLocation}

commit c5d5900d495ccf8073407ba276e9fc669164a488

Fix incorrect navbar width
Regression in PR #17460

frontend/src/metabase/nav/containers/Navbar.jsx
@@ -216,7 +216,7 @@ export default class Navbar extends Component {
           </Link>
         </Flex>
         <Flex className="flex-full z1" pr={2} align="center">
-          <Box width={1} style={{ maxWidth: 512 }}>
+          <Box width={1} style={{ maxWidth: 500 }}>
             <SearchBar
               location={this.props.location}
               onChangeLocation={this.props.onChangeLocation}